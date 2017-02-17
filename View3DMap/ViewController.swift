0c1c9aee2c98290728348eda7f1e0d0dce752cc5
Vodafone Care <jayzalawadia@jays-macbook-pro-2.local>
2017-02-17 13:52:56 -0500

    Initial Commit

---
+++ View3DMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  View3DMap
+//
+//  Created by Jay Zalawadia on 2/17/17.
+//  Copyright © 2017 Jay Zalawadia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
